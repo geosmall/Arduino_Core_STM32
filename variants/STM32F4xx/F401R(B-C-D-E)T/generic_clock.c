bbd161d8757f3a352d3ffbe010f42cf23617fd30
geosmall <geomall92@gmail.com>
2024-02-14 07:53:54 -0500

    Restructured variants

--- variants/STM32F4xx/F401R(B-C-D-E)T/generic_clock.c
+++ variants/STM32F4xx/F401R(B-C-D-E)T/generic_clock.c
@@ -1,15 +1,3 @@
-/*
- *******************************************************************************
- * Copyright (c) 2020-2021, STMicroelectronics
- * All rights reserved.
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- *******************************************************************************
- */
 #if defined(ARDUINO_GENERIC_F401RBTX) || defined(ARDUINO_GENERIC_F401RCTX) ||\
     defined(ARDUINO_GENERIC_F401RDTX) || defined(ARDUINO_GENERIC_F401RETX)
 #include "pins_arduino.h"
--- variants/STM32F4xx/FC_Revo_F405RG/variant_FC_REVO_F405RG.cpp
+++ variants/STM32F4xx/FC_Revo_F405RG/variant_FC_REVO_F405RG.cpp
@@ -1,25 +1,13 @@
-/*
- *******************************************************************************
- * Copyright (c) 201-2021, STMicroelectronics
- * All rights reserved.
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- *******************************************************************************
- */
-#if defined(ARDUINO_NUCLEO_F401RE)
+#if defined(ARDUINO_FC_REVO_F405RG)
 
 #include "stm32_def.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 /**
-  * @brief  System Clock Configuration
-  * @param  None
+  * @brief System Clock Configuration
   * @retval None
   */
 WEAK void SystemClock_Config(void)
@@ -30,7 +18,7 @@ WEAK void SystemClock_Config(void)
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -40,9 +28,9 @@ WEAK void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLN = 168;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -54,10 +42,10 @@ WEAK void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
@@ -67,4 +55,4 @@ WEAK void SystemClock_Config(void)
 }
 #endif
 
-#endif /* ARDUINO_NUCLEO_F401RE */
+#endif // defined(ARDUINO_FC_REVO_F405RG)
