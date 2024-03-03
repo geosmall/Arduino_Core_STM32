5257364be750be0d55c6bfedb83c2de9419df18e
geosmall <geomall92@gmail.com>
2024-03-03 08:42:40 -0500

    Added DISCO_F407VG variant

--- variants/STM32F4xx/F401R(B-C-D-E)T/variant_NUCLEO_F401RE.cpp
+++ variants/STM32F4xx/F401R(B-C-D-E)T/variant_NUCLEO_F401RE.cpp
@@ -1,15 +1,3 @@
-/*
- *******************************************************************************
- * Copyright (c) 201-2021, STMicroelectronics
- * All rights reserved.
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- *******************************************************************************
- */
 #if defined(ARDUINO_NUCLEO_F401RE)
 
 #include "stm32_def.h"
